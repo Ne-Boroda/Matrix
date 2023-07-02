455c35c216331b3527c00c1b8d9a913583385212
Nikita Bondarenko <nikbondik00@gmail.com>
2023-07-02 15:07:00 +0300

    Add & Fill Matrix

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Matrix
+{
+    private int rows;
+    private int cols;
+    private int[,] matrix;
+
+    public Matrix(int rows, int cols)
+    {
+        this.rows = rows;
+        this.cols = cols;
+        matrix = new int[rows, cols];
+    }
+
+    public void CreateMatrix()
+    {
+        Random rnd = new Random();
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rnd.Next(10);
+            }
+        }
+    }
+
+    public int this[int i, int j]
+    {
+        get { return matrix[i, j]; }
+        set { matrix[i, j] = value; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        Matrix matrix = new Matrix(rows, cols); // создание матрицы
+
+        matrix.CreateMatrix(); // заполнение матрицы случайными значениями
+
+        // вывод матрицы
+        Console.WriteLine("Матрица:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
